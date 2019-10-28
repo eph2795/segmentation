b3dea9b16f3e315d20459fe0db3994900ae89b67
Efim Lavrukhin <evlavrukhin@gpu03.o3.ru>
2019-10-28 14:55:29 +0300

    finally all exps done + predicted stacks

---
+++ experiments/carbRNF_dim0_train_dim2_test_exp/exp_config.py
@@ -0,0 +1,59 @@
+data_conf = {
+    'conf_name': 'carbRNF_dim0_train_dim2_test_exp',
+    'stacks': [
+        {
+            'path': '../../data/carbRNF',
+            'slice_train': (slice(None), slice(None), slice(230)),
+            'slice_val': (slice(None), slice(None), slice(250, 470)),
+        },
+        {
+            'path': '../../data/carb96558',
+            'slice_test': (slice(None), slice(None), slice(490, None)),
+        },
+        {
+            'path': '../../data/carb71',
+            'slice_test': (slice(None), slice(None), slice(490, None)),
+        },
+        {
+            'path': '../../data/carbRNF',
+            'slice_test': (slice(None), slice(None), slice(470, None)),
+        },
+        {
+            'path': '../../data/SPE_carb10_58_box3',
+            'slice_test': (slice(None), slice(None), slice(280, None)),
+        },
+        {
+            'path': '../../data/SoilAh-1',
+            'slice_test': (slice(None), slice(None), slice(470, None)),
+        },
+        {
+            'path': '../../data/SoilB-2',
+            'slice_test': (slice(None), slice(None), slice(470, None)),
+        },
+        {
+            'path': '../../data/TeTree_subset1',
+            'slice_test': (slice(None), slice(None), slice(480, None)),
+        },
+        {
+            'path': '../../data/TiTree_subset2',
+            'slice_test': (slice(None), slice(None), slice(480, None)),
+        },
+        {
+            'path': '../../data/Urna_22',
+            'slice_test': (slice(None), slice(None), slice(480, None)),
+        },
+        {
+            'path': '../../data/Urna_30',
+            'slice_test': (slice(None), slice(None), slice(480, None)),
+        },
+        {
+            'path': '../../data/Urna_34',
+            'slice_test': (slice(None), slice(None), slice(470, None)),
+        },
+    ],
+    'patches': {
+        'train': (1, 128, 128),
+        'val': (1, 128, 128),
+        'test': (128, 128, 1),
+    },
+}
